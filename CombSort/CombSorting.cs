d83f0c95681110d0380be5b8b868800204f84b69
Yurij <horyuriy@gmail.com>
2023-08-03 20:20:27 +0300

    Comb

---
+++ CombSort/CombSorting.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombSort
+{
+    internal class CombSorting
+    {
+        static void Swap(ref int value1, ref int value2)
+        {
+            var temp = value1;
+            value1 = value2;
+            value2 = temp;
+        }
+
+        //метод для генерации следующего шага
+        static int GetNextStep(int s)
+        {
+            s = s * 1000 / 1247;
+            return s > 1 ? s : 1;
+        }
+        public static int[] CombSort(int[] array)
+        {
+            var arrayLength = array.Length;
+            var currentStep = arrayLength - 1;
+
+            while (currentStep > 1)
+            {
+                for (int i = 0; i + currentStep < array.Length; i++)
+                {
+                    if (array[i] > array[i + currentStep])
+                    {
+                        Swap(ref array[i], ref array[i + currentStep]);
+                    }
+                }
+
+                currentStep = GetNextStep(currentStep);
+            }
+
+            //сортировка пузырьком
+            for (var i = 1; i < arrayLength; i++)
+            {
+                var swapFlag = false;
+                for (var j = 0; j < arrayLength - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                        swapFlag = true;
+                    }
+                }
+
+                if (!swapFlag)
+                {
+                    break;
+                }
+            }
+
+            return array;
+        }
+
+        //метод для получения массива заполненного случайными числами
+        static int[] GetRandomArray(int length, int minValue, int maxValue)
+        {
+            var r = new Random();
+            var outputArray = new int[length];
+            for (var i = 0; i < outputArray.Length; i++)
+            {
+                outputArray[i] = r.Next(minValue, maxValue);
+            }
+
+            return outputArray;
+        }
+    }
+}
---
+++ CombSort/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CombSort
+{
+    internal class Program
+    {
+        static int[] GetRandomArray(int length, int minValue, int maxValue)
+        {
+            var r = new Random();
+            var outputArray = new int[length];
+            for (var i = 0; i < outputArray.Length; i++)
+            {
+                outputArray[i] = r.Next(minValue, maxValue);
+            }
+
+            return outputArray;
+        }
+        static void Main(string[] args)
+        {
+            var arr = GetRandomArray(15, 0, 10);
+            Console.WriteLine("Входные данные: {0}", string.Join(", ", arr));
+            Console.WriteLine("Отсортированный массив: {0}", string.Join(", ", CombSorting.CombSort(arr)));
+            Console.ReadLine();
+        }
+    }
+}
