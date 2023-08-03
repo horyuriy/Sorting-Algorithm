cd9e36e7b88607152abac9746b50a67c8f54ddff
Yurij <horyuriy@gmail.com>
2023-08-03 19:13:40 +0300

    Add Pancake

---
+++ PancakeSort/PancakeSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PancakeSort
+{
+    public class PancakeSort
+    {
+        public static int IndexOfMax(int[] array, int n)
+        {
+            int result = 0;
+            for (var i = 1; i <= n; ++i)
+            {
+                if (array[i] > array[result])
+                {
+                    result = i;
+                }
+            }
+            return result;
+        }
+        public static void Flip(int[] array, int end)
+        {
+            for (var start = 0; start < end; start++, end--)
+            {
+                var temp = array[start]; array[start] = array[end]; array[end] = temp;
+
+            }
+        }
+        public static int[] PancakeSorts(int[] array)
+        {
+            for (var subArrayLength = array.Length-1; subArrayLength >= 0; subArrayLength--)
+            {
+                var indexOfMax=IndexOfMax(array,subArrayLength);
+                if(indexOfMax!= subArrayLength)
+                {
+                    Flip(array, indexOfMax);
+                    Flip(array, subArrayLength);
+                }
+            }
+            return array;
+        }
+    }
+}
---
+++ PancakeSort/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace PancakeSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Random sorting");
+            Console.WriteLine("Enter the array elements:");
+            var part = Console.ReadLine().Split(new[] { " ", ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
+            var array = new int[part.Length];
+            for (int i = 0; i < part.Length; i++)
+            {
+                array[i] = Convert.ToInt32(part[i]);
+            }
+            Console.WriteLine("Sorted array: {0}", string.Join(", ", PancakeSort.PancakeSorts(array)));
+            Console.ReadLine();
+        }
+    }
+}
